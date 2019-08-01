47b1182277044cc2d31dc569051ea2bd07cd3f82
AmAdev <aaananchai@gmail.com>
2019-08-01 13:13:56 +0700

    MenuBar and extension of BaseCell for readable

--- Youtube/Youtube/View/MenuBar.swift
+++ Youtube/Youtube/View/MenuBar.swift
@@ -8,18 +8,90 @@
 
 import UIKit
 
-class MenuBar: UIView {
+class MenuBar: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31)
+        cv.delegate = self
+        cv.dataSource = self
+        return cv
+    }()
+    
+    let cellId = "CellId"
+    let imageName = ["home", "trending", "subscriptions", "account"]
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setUpView()
+       
+        collectionView.register(MenuCell.self, forCellWithReuseIdentifier: cellId)
+        addSubview(collectionView)
+        
+        addConstraintWithFormat(format: "H:|[v0]|", views: collectionView)
+        addConstraintWithFormat(format: "V:|[v0]|", views: collectionView)
+        
+        let seletedIndexPath = IndexPath(item: 0, section: 0)
+        collectionView.selectItem(at: seletedIndexPath, animated: false, scrollPosition: .bottom)
+   
+    }
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
     }
     
-    func setUpView() {
-        backgroundColor = UIColor.rgb(red: 230, green: 32, blue: 31)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MenuCell
+        cell.imageView.image = UIImage(named: imageName[indexPath.item])?.withRenderingMode(.alwaysTemplate)
+        cell.tintColor = UIColor.rgb(red: 94, green: 14, blue: 13)
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width/4, height: frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+class MenuCell: BaseCell {
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "home")
+        return imageView
+    }()
+    
+    override var isHighlighted: Bool {
+        didSet {
+            imageView.tintColor = isHighlighted ? UIColor.white : UIColor.rgb(red: 94, green: 14, blue: 13)
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            imageView.tintColor = isSelected ? UIColor.white : UIColor.rgb(red: 94, green: 14, blue: 13)
+        }
+    }
+    
+    
+
+    
+    override func setUpViews() {
+        super.setUpViews()
+        addSubview(imageView)
+        addConstraintWithFormat(format: "H:[v0(28)]", views: imageView)
+        addConstraintWithFormat(format: "V:[v0(28)]", views: imageView)
+        
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+    }
 }
