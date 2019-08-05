130dee192713a718a57185cef3d42dc9663cf353
AmAdev <aaananchai@gmail.com>
2019-08-05 22:30:51 +0700

    animation collectionView

--- Youtube/Youtube/Controller/SettingLauncher.swift
+++ Youtube/Youtube/Controller/SettingLauncher.swift
@@ -11,8 +11,16 @@ import UIKit
 class SettingLauncher: NSObject {
     override init() {
         super.init()
-        ShowSetting()
     }
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        
+        return cv
+    }()
+    
     
     let blackView = UIView()
     
@@ -26,13 +34,23 @@ class SettingLauncher: NSObject {
             blackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleDismiss)))
             
             window.addSubview(blackView)
+            window.addSubview(collectionView)
+            
+            let height: CGFloat = 200
+            let y = window.frame.height - height
+            collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
+            
             blackView.frame = window.frame
             blackView.alpha = 0
             
-            UIView.animate(withDuration: 0.5) {
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                
                 self.blackView.alpha = 1
-            }
-            
+                
+                self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+                
+            }, completion: nil)
+        
         }
         
     }
@@ -41,6 +59,10 @@ class SettingLauncher: NSObject {
     @objc func handleDismiss() {
         UIView.animate(withDuration: 0.5) {
             self.blackView.alpha = 0
+            
+            if let window = UIApplication.shared.keyWindow {
+                self.collectionView.frame = CGRect(x: 0, y: window.frame.height, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            }
         }
     }
     
